61b9db5a5203bf2845cf597bb6a43e6ea35b95ad
Keiwan Donyagard <keiwan@gmx.de>
2018-04-30 00:15:56 +0200

    Playing around with PEG.js grammar

--- resources/js/scheduler.js
+++ resources/js/scheduler.js
@@ -1,26 +1,113 @@
-//alert(parser.parse("5 + 5"));
 
-var Scheduler = {
+var Scheduler = (function(){
 
-	elements: {
-		dslInput: document.querySelector("input[name=dsl-cmd-input]"),
-		scheduleContainer: document.querySelector("#schedule-container")
-	},
+	var grammar = `
 
-	init: function() {
-		this.bindUIActions();
-	},
-
-	bindUIActions: function() {
-
-		var inp = this.elements.dslInput;
-		var container = this.elements.scheduleContainer;
-
-		inp.oninput = function(){
-			
-			container.innerHTML = parser.parse(inp.value);
+		FullInput
+		  = head:Day start:Time _? "-" _? end:Time { return {
+		  	day: head,
+		  	startTime: start,
+		  	endTime: end
+		  }; 
 		}
-	}
-};
 
-Scheduler.init();
+		Expression
+		  = head:Term tail:(_ ("+" / "-") _ Term)* {
+		      return tail.reduce(function(result, element) {
+		        if (element[1] === "+") { return result + element[3]; }
+		        if (element[1] === "-") { return result - element[3]; }
+		      }, head);
+		    }
+
+		Term
+		  = head:Factor tail:(_ ("*" / "/") _ Factor)* {
+		      return tail.reduce(function(result, element) {
+		        if (element[1] === "*") { return result * element[3]; }
+		        if (element[1] === "/") { return result / element[3]; }
+		      }, head);
+		    }
+
+		Factor
+		  = "(" _ expr:Expression _ ")" { return expr; }
+		  / Integer
+
+		Time 
+		  = head:Integer([,.:]Integer)? {
+		  	return parseFloat(head.replace(",",".").replace(":","."));
+		  }
+
+		Day
+		  = Monday / Tuesday / Wednesday / Thursday / Friday / Saturday / Sunday
+
+		Monday
+		  = "Mon"i"day"i? { return 1; }
+
+		Tuesday
+		  = "Tue"i"sday"i? { return 2; }
+
+		Wednesday
+		  = "Wed"i"nesday"i? { return 3; }
+
+		Thursday
+		  = "Thu"i"rsday"i? { return 4; }
+
+		Friday
+		  = "Fri"i"day"i? { return 5; }
+
+		Saturday
+		  = "Sat"i"urday"i? { return 6; }
+
+		Sunday
+		  = "Sun"i"day"i? { return 7; }
+
+		Integer "integer"
+		  = _ [0-9]+ { return parseInt(text(), 10); }
+
+		_ "whitespace"
+		  = [ \\t\\n\\r]*
+	`;
+
+	var parser = peg.generate(grammar);
+
+	var elem;
+	return {
+
+		elements: {
+			dslInput: document.querySelector("input[name=dsl-cmd-input]"),
+			scheduleContainer: document.querySelector("#schedule-container")
+		},
+
+		init: function() {
+			
+			elem = this.elements;
+			this.bindUIActions();
+			return this;
+		},
+
+		bindUIActions: function() {
+
+			var inp = elem.dslInput;
+			var handler = this.handleInputChange;
+
+			inp.oninput = function(){
+				
+				handler(inp.value);
+				//elem.scheduleContainer.innerHTML = parser.parse(inp.value);
+			}
+		},
+
+		handleInputChange: function(input) {
+
+			try {
+
+				elem.scheduleContainer.innerHTML = parser.parse(input);
+					
+			} catch (e) {
+				//console.log(e);
+				elem.scheduleContainer.innerHTML = e;
+			}
+		}
+
+	}.init();
+
+})();
