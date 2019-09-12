975fe06f6bd38b23e2e5b70d196f5b642cf10257
Keiwan Donyagard <keiwan@gmx.de>
2019-09-12 23:04:48 +0200

    Fixed lookAt and projection matrices

--- 404/shaders.js
+++ 404/shaders.js
@@ -24,7 +24,6 @@ const unlitColorFragment = `
 
   void main() {
     gl_FragColor = v_color;
-    // gl_FragColor = vec4(1, 0, 0, 0);
   }
 `
 
@@ -55,27 +54,67 @@ const unlitTextureFragment = `
   }
 `
 
-const waveVertex = `
+const waterVertex = `
   precision highp float;
+
+  uniform mat4 u_model;
+  uniform mat4 u_view;
+  uniform mat4 u_proj;
+  uniform float u_time;
 
   attribute vec4 a_position;
 
+  varying vec4 pos;
+
+  float rand(vec2 co){
+	
+    return fract(sin(dot(co.xy , vec2(12.9898, 78.233))) * 43758.5453);
+  }
+
   void main() {
-    gl_Position = a_position;
+    gl_Position = a_position.xzyw;
+    pos = u_proj * (u_view * (u_model * a_position));
+    // pos = (u_view * (u_model * a_position));
+    // pos.z = abs(pos.z);
+    gl_Position = pos;
+    // gl_Position.z = abs(gl_Position.z);
+    // gl_Position = mix(a_position.xzyw, pos, u_time * 0.000001);
   }
 `
 
-const waveFragment = `
+const waterFragment = `
   precision highp float;
 
+  const vec4 LIGHT_BLUE = vec4(0.3058823529, 0.8588235294, 0.9294117647, 1);
+  const vec4 DARK_BLUE = vec4(0.0275, 0.2235294118, 0.2509803922, 1);
+
+  varying vec4 pos;
+    
   void main() {
-    gl_FragColor = vec4(0.431, 0.87, 0.9, 1.0);
+
+    gl_FragColor = DARK_BLUE;
+    // gl_FragColor = vec4(pos.xyz, 1.0) + vec4(0.2, 0.2, 0.2, 0);
+
+    // x = red
+    // gl_FragColor = vec4(pos.x, 0.0, 0.0, 1.0);
+
+    // y = green
+    // gl_FragColor = vec4(0.0, pos.y, 0.0, 1.0);
+
+    // z = blue
+    // gl_FragColor = vec4(0.0, 0.0, pos.z, 1.0);
+    // gl_FragColor = vec4(0.0, 0.0, abs(pos.z), 1.0);
+
+    // w = red
+    // gl_FragColor = vec4(pos.w, 0.0, 0.0, 1.0);
+
+    // gl_FragColor = DARK_BLUE;
   }
 
 `
 
 export { 
-  waveVertex, waveFragment,
+  waterVertex, waterFragment,
   unlitColorVertex, unlitColorFragment,
   unlitTextureVertex, unlitTextureFragment
 }
--- 404/waves-404.js
+++ 404/waves-404.js
@@ -1,26 +1,146 @@
 import {
-  waveVertex, waveFragment, 
+  waterVertex, waterFragment, 
   unlitColorVertex, unlitColorFragment, 
   unlitTextureVertex, unlitTextureFragment
 } from './shaders.js'
 
 const Colors = {
   lightPink: [0.9803921569, 0.8705882353, 0.8705882353, 1],
-  darkPink: [1, 0.7411764706, 0.7490196078, 1]
+  darkPink: [1, 0.7411764706, 0.7490196078, 1],
+  lightBlue: [0.3058823529, 0.8588235294, 0.9294117647, 1],
+  darkBlue: [0.0275, 0.2235294118, 0.2509803922, 1]
 }
 
-const identityMat = new Float32Array([
-  1, 0, 0, 0,
-  0, 1, 0, 0,
-  0, 0, 1, 0,
-  0, 0, 0, 1
-])
-const scale2Mat = new Float32Array([
-  2, 0, 0, 0,
-  0, 2, 0, 0,
-  0, 0, 2, 0,
-  0, 0, 0, 1
-])
+const WATER_RESOLUTION = 3
+
+const identityMat = createScaleMat(1, 1, 1)
+
+const skyScaleMat = createScaleMat(2, 1.75, 2)
+const skyTranslationMat = createTranslationMat(0, 0.25, 0)
+
+/**
+ * @returns {Float32Array}
+ */
+function createScaleMat(scaleX, scaleY, scaleZ = 1) {
+  return new Float32Array([
+    scaleX, 0, 0, 0,
+    0, scaleY, 0, 0,
+    0, 0, scaleZ, 0,
+    0, 0, 0, 1
+  ])
+}
+
+/**
+ * @returns {Float32Array}
+ */
+function createTranslationMat(tx, ty, tz = 1) {
+  return new Float32Array([
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    tx, ty, tz, 1
+  ])
+}
+
+/**
+ * @param {Vector3} position 
+ * @param {Vector3} target 
+ * @param {Vector3} up 
+ */
+function createLookAt(position, target, up) {
+  
+  let f = target.minus(position).normalized()
+  const s = f.cross(up).normalized()
+  const u = s.cross(f)
+
+  return new Float32Array([
+    s.x, u.x, -f.x, 0,
+    s.y, u.y, -f.y, 0,
+    s.z, u.z, -f.z, 0,
+    -s.dot(position), -u.dot(position), f.dot(position), 1
+  ])
+}
+
+/**
+ * @param {number} n The near plane
+ * @param {number} f The far plane
+ * @param {number} r The near right edge
+ * @param {number} t The near top edge
+ */
+function createProjection(n, f, r, t) {
+
+  // return new Float32Array([
+  //   n / r, 0, 0, 0,
+  //   0, n / t, 0, 0,
+  //   0, 0, -(f + n)/(f - n), -2 * f * n / (f - n),
+  //   0, 0, -1, 0
+  // ])
+  return new Float32Array([
+    n / r, 0, 0, 0,
+    0, n / t, 0, 0,
+    0, 0, -(f + n)/(f - n), -1,
+    0, 0, -2 * f * n / (f - n), 0
+  ])  
+}
+
+class Vector3 {
+  constructor(x, y, z) {
+    /** @member {number} */
+    this.x = x
+    /** @member {number} */
+    this.y = y
+    /** @member {number} */
+    this.z = z
+  }
+
+  /**
+   * @param {Vector3} other 
+   * @returns {Vector3}
+   */
+  cross(other) {
+    return new Vector3(
+      this.y * other.z - this.z * other.y,
+      this.z * other.x - this.x * other.z,
+      this.x * other.y - this.y * other.x,
+    )
+  }
+
+  /**
+   * @param {Vector3} other 
+   * @returns {Vector3}
+   */
+  dot(other) {
+    return this.x * other.x + this.y * other.y + this.z * other.z
+  }
+
+  /**
+   * @param {Vector3} other
+   * @returns {Vector3}
+   */
+  minus(other) {
+    return new Vector3(this.x - other.x, this.y - other.y, this.z - other.z)
+  }
+
+  /**
+   * @param {number} scalar 
+   * @returns {Vector3}
+   */
+  divide(scalar) {
+    return new Vector3(this.x / scalar, this.y / scalar, this.z / scalar)
+  }
+
+  /** @returns {number} */
+  length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
+  }
+
+  /** @returns {Vector3} */
+  normalized() {
+    const len = this.length()
+    if (len === 0) return this
+    return this.divide(len)
+  }
+}
 
 
 class Geometry {
@@ -41,9 +161,9 @@ class Geometry {
   constructor(vertices, indices) {
     this._bufferData = new Float32Array(vertices.map(v => v.data).flat())
     if (!indices) {
-      this._indices = new Float32Array(Array(this._bufferData.length).keys())
+      this._indices = new Uint16Array(Array(this._bufferData.length).keys())
     } else {
-      this._indices = new Uint8Array(indices)
+      this._indices = new Uint16Array(indices)
     }
   }
 
@@ -77,49 +197,73 @@ class Vertex {
 
 class Wave404Renderer {
 
-  /** @returns {WebGLRenderingContext} */
-  get gl() {
-    return this._gl
-  } 
-
-  /** @returns {HTMLCanvasElement} */
-  get canvas() {
-    return this._canvas
-  }
-
-  /** @returns {WebGLProgram} */
-  get unlitShader() {
-    return this._unlitShader
-  }
-
-  /** @returns {WebGLProgram} */
-  get unlitTextureShader() {
-    return this._unlitTextureShader
-  }
-
-  /** @returns {WebGLProgram} */
-  get waterShader() {
-    return this._waterShader
-  }
-
-  /** @returns {Geometry} */
-  get skyGeometry() {
-    return this._skyGeometry
-  }
-
   constructor(canvas) {
-    this._canvas = canvas
-    this._gl = canvas.getContext('webgl')
-    this._waterShader = this._createProgram(this.gl, waveVertex, waveFragment)
-    this._unlitShader = this._createProgram(this.gl, unlitColorVertex, unlitColorFragment)
-    this._unlitTextureShader = this._createProgram(this.gl, unlitTextureVertex, unlitTextureFragment)
-    this._skyGeometry = this._createSkyGeometry()
-    this._skyGeometry.upload(this._gl)
+
+    /**
+     * @member {HTMLCanvasElement}
+     */
+    this.canvas = canvas
+    /**
+     * @member {WebGLRenderingContext}
+     */
+    this.gl = canvas.getContext('webgl')
+
+    /**
+     * @member {WebGLShader}
+     */
+    this.waterShader = this._createProgram(this.gl, waterVertex, waterFragment)
+    /**
+     * @member {WebGLShader}
+     */
+    this.unlitShader = this._createProgram(this.gl, unlitColorVertex, unlitColorFragment)
+    /**
+     * @member {WebGLShader}
+     */
+    this.unlitTextureShader = this._createProgram(this.gl, unlitTextureVertex, unlitTextureFragment)
+
+    /**
+     * @member {Geometry}
+     */
+    this.skyGeometry = this._createSkyGeometry()
+    this.skyGeometry.upload(this.gl)
+    /** 
+     * @member {Geometry} 
+     */
+    this.waterGeometry = this._createWaterGeometry()
+    this.waterGeometry.upload(this.gl)
 
     // console.log(this._skyGeometry._bufferData.length)
 
     canvas.width = canvas.clientWidth
     canvas.height = canvas.clientHeight
+
+    /** @member {number} */
+    this.time = 0
+
+    // this.projMat = new Float32Array([
+    //   2 / canvas.width, 0, 0, 0,
+    //   0, 2 / canvas.height, 0, 0,
+    //   0, 0, 1, 0,
+    //   0, 0, 0, 1
+    // ])
+    // this.projMat = createProjection(0.1, 10, 0.5 * canvas.width, 0.5 * canvas.height)
+    this.projMat = createProjection(0.1, 10, 0.4, 0.4)
+    // this.projMat = identityMat
+    console.log(this.projMat)
+
+    this.viewMat = createLookAt(
+      new Vector3(0, 1, -1),
+      new Vector3(0, 0, 0),
+      new Vector3(0, 1, 1)
+    )
+    // this.viewMat = identityMat
+    // this.viewMat = new Float32Array([
+    //   1, 0, 0, 0,
+    //   0, 0, 1, 0,
+    //   0, 1, 0, 0,
+    //   0, 0, 0, 1
+    // ])
+    console.log(this.viewMat)
   }
 
   render(deltaTime) {
@@ -139,41 +283,79 @@ class Wave404Renderer {
     gl.clearColor(...Colors.lightPink, 1)
     gl.clear(gl.COLOR_BUFFER_BIT)
 
-    const projMat = new Float32Array([
-      2 / canvas.width, 0, 0, 0,
-      0, 2 / canvas.height, 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1
-    ])
-    
     // Render sky background
-    let program = this._unlitShader
+
+    let program = this.unlitShader
     gl.useProgram(program)
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.skyGeometry.buffer)
-    const positionIndex = gl.getAttribLocation(program, "a_position")
+    let positionIndex = gl.getAttribLocation(program, "a_position")
     const colorIndex = gl.getAttribLocation(program, "a_color")
     gl.vertexAttribPointer(positionIndex, 4, gl.FLOAT, false, 32, 0)
     gl.vertexAttribPointer(colorIndex, 4, gl.FLOAT, false, 32, 16)
     gl.enableVertexAttribArray(positionIndex)
     gl.enableVertexAttribArray(colorIndex)
-
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_proj"), false, scale2Mat)
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_view"), false, identityMat)
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_model"), false, identityMat)
-
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.skyGeometry.indices)
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_proj"), false, skyScaleMat)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_view"), false, skyTranslationMat)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_model"), false, identityMat)
     
-    // gl.drawArrays(gl.TRIANGLES, 0, 6)
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 0)
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
 
     // Render waves
+
+    program = this.waterShader
+    gl.useProgram(program)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.waterGeometry.buffer)
+    positionIndex = gl.getAttribLocation(program, "a_position")
+    gl.vertexAttribPointer(positionIndex, 4, gl.FLOAT, false, 0, 0)
+    gl.disableVertexAttribArray(colorIndex)
+    gl.enableVertexAttribArray(positionIndex)
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.waterGeometry.indices)
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_proj"), false, this.projMat)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_view"), false, this.viewMat)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_model"), false, createScaleMat(2, 2, 2))
+    // gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_model"), false, identityMat)
+    
+    gl.uniform1f(gl.getUniformLocation(program, "u_time"), this.time)
+
+    gl.drawElements(gl.TRIANGLES, this.waterGeometry._indices.length, gl.UNSIGNED_SHORT, 0)
+
     // Render overlay 404 message
+
+    this.time += deltaTime
   }
 
   /** @returns {Geometry} */
   _createSkyGeometry() {
     return this._createVerticalGradientQuad(Colors.darkPink, Colors.lightPink)
+  }
+
+  /** @returns {Geometry} */
+  _createWaterGeometry() {
+
+    const mappedIdx = (x, y) => {
+      return WATER_RESOLUTION * y + x
+    }
+
+    var vertices = []
+    var indices = []
+    for (let y = 0; y < WATER_RESOLUTION; y++) {
+      for (let x = 0; x < WATER_RESOLUTION; x++) {
+        
+        vertices.push(new Vertex(-0.5 + (x / (WATER_RESOLUTION - 1)), 0, -0.5 + (y / (WATER_RESOLUTION - 1)), 1))
+        // vertices.push(new Vertex(-0.5 + (x / (WATER_RESOLUTION - 1)), -0.5 + (y / (WATER_RESOLUTION - 1)), 0, 1))
+
+        if (y !== WATER_RESOLUTION - 1 && x !== WATER_RESOLUTION - 1) {
+          indices.push(mappedIdx(x, y), mappedIdx(x, y + 1), mappedIdx(x + 1, y + 1))
+          indices.push(mappedIdx(x + 1, y), mappedIdx(x, y), mappedIdx(x + 1, y + 1))
+        }  
+      }
+    }
+    return new Geometry(vertices, indices)
   }
 
   /**
@@ -186,17 +368,7 @@ class Wave404Renderer {
       new Vertex(-0.5, -0.5, 0, 1, ...bottomColor),
       new Vertex(0.5, -0.5, 0, 1, ...bottomColor),
       new Vertex(-0.5, 0.5, 0, 1, ...topColor),
-      new Vertex(0.5, 0.5, 0, 1, ...topColor),
-      // new Vertex(-0.5, -0.5, 0, 1, ...bottomColor),
-      // new Vertex(0.5, -0.5, 0, 1, ...bottomColor),
-      // new Vertex(-0.5, 0.5, 0, 1, ...topColor),
-      // new Vertex(-0.5, 0.5, 0, 1, ...topColor),
-      // new Vertex(0.5, -0.5, 0, 1, ...bottomColor),
-      // new Vertex(0.5, 0.5, 0, 1, ...topColor),
-      // new Vertex(-0.5, -0.5, 0, 1),
-      // new Vertex(0.5, -0.5, 0, 1),
-      // new Vertex(-0.5, 0.5, 0, 1),
-      // new Vertex(0.5, 0.5, 0, 1),
+      new Vertex(0.5, 0.5, 0, 1, ...topColor)
     ],[0, 1, 2, 2, 1, 3])
   }
 
@@ -242,9 +414,10 @@ const canvas = document.querySelector('canvas')
 const renderer = new Wave404Renderer(canvas)
 
 let time = 0
-function renderLoop (now) {
+function renderLoop (now = 0) {
 
-  if (!time) time = now
+  if (!time) 
+    time = now
   var elapsed = now - time
 
   renderer.render(elapsed)
